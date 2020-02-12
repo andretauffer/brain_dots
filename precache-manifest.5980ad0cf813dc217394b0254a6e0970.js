bc0f9d41c60eebbd7dd43a4c061a100130c0e78b
andretauffer <andregiraotauffer@gmail.com>
2020-02-12 21:54:13 +0100

    Updates

---
+++ precache-manifest.5980ad0cf813dc217394b0254a6e0970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f8cefbb3d1425463ee025ff9fe6a4b",
+    "url": "/brain_dots/index.html"
+  },
+  {
+    "revision": "4ed5e8b00e32a7c62007",
+    "url": "/brain_dots/static/css/main.e11d61cd.chunk.css"
+  },
+  {
+    "revision": "8fd50cdd348191d30ba7",
+    "url": "/brain_dots/static/js/2.fdee5e75.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/brain_dots/static/js/2.fdee5e75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed5e8b00e32a7c62007",
+    "url": "/brain_dots/static/js/main.8153994a.chunk.js"
+  },
+  {
+    "revision": "49e7559f133022241bbf",
+    "url": "/brain_dots/static/js/runtime-main.181156da.js"
+  }
+]);
